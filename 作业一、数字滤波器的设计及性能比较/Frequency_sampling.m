965e1d1fa180d9b7d37957986f9e311337a85269
Liferry <35103759+Liferry@users.noreply.github.com>
2019-06-17 15:53:32 +0800

    1

---
+++ 作业一、数字滤波器的设计及性能比较/Frequency_sampling.m
@@ -0,0 +1,39 @@
+clear;
+
+a = 1;
+b = 0.7;
+f1 = 100;
+f2 = 200;
+mean = 0;
+var = 2.5;
+fs = 1000;
+N = 200;
+y = a * sin(2 * pi * f1 / fs * (0: 1: N - 1)) + ...
+    b * cos(2 * pi * f2 / fs * (0: 1: N - 1)) + ...
+    normrnd(mean, sqrt(var));
+
+fc = 150;
+wc = 2 * pi * fc / fs;
+NN = 10;
+N_freq_sample_fir = 100;
+M = fix(wc * (NN  - 1) / pi) + 1;
+f = linspace(0, 1, NN);
+a = [ones(1, M) zeros(1, NN - M)];
+FIR1 = fir2(N_freq_sample_fir, f, a);
+
+out1 = filter(FIR1, 1, y);
+
+figure;
+subplot(2, 2, 1);
+plot(0: N - 1, y, '-');
+title('时域-滤波前信号');
+subplot(2, 2, 2);
+plot(0: N - 1, out1, '-');
+title('时域-滤波后信号');
+subplot(2, 2, 3);
+plot(0: N - 1, abs(fft(y)));
+title('频域-滤波前信号');
+subplot(2, 2, 4);
+plot(0: N - 1, abs(fft(out1)));
+title('频域-滤波后信号');
+
